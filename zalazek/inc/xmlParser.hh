1ee88e028d34e328d096b783ed785631e143b884
chedoska <259354@student.pwr.edu.pl>
2023-11-20 21:54:04 +0100

    Dodanie pliku parsera

---
+++ zalazek/inc/xmlParser.hh
@@ -0,0 +1,21 @@
+#include <xercesc/sax2/SAX2XMLReader.hpp>
+#include <xercesc/sax2/XMLReaderFactory.hpp>
+#include <xercesc/sax2/DefaultHandler.hpp>
+#include <xercesc/util/XMLString.hpp>
+#include "xmlinterp.hh"
+#include <iostream>
+#include <list>
+
+#include "Configuration.hh"
+
+using namespace xercesc;
+
+/*!
+ * Czyta z pliku opis poleceń i dodaje je do listy komend,
+ * które robot musi wykonać.
+ * \param sFileName - (\b we.) nazwa pliku z opisem poleceń.
+ * \param CmdList - (\b we.) zarządca listy poleceń dla robota.
+ * \retval true - jeśli wczytanie zostało zrealizowane poprawnie,
+ * \retval false - w przeciwnym przypadku.
+ */
+bool ReadFile(const char* sFileName, Configuration &rConfig);
--- zalazek/src/main.cpp
+++ zalazek/src/main.cpp
@@ -4,11 +4,15 @@
 
 #include "AbstractInterp4Command.hh"
 #include "ProgramInterpreter.hh"
+#include "xmlParser.hh"
 
 using namespace std;
 
 int main()
 {
+  Configuration   Config;
+   if (!ReadFile("config/config.xml",Config)) return 1;
+
   ProgramInterpreter programInterpreter;
   if(!programInterpreter.ExecProgram("/home/chedoska/Pulpit/opis_dzialan.cmd")){
     std::cout << "BŁĄD: Koniec programu!\n\n\n";
